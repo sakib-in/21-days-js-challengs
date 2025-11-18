f63bb51890e248b83c5c9fa0428b7d79eb9d08ad
Sakib Hussain <sakibhussain.in@gmail.com>
2025-11-18 18:14:20 +0530

    Update and rename classQuestions.js to solution.js

---
+++ Day04B_digits-and-numbers/solution.js
@@ -0,0 +1,177 @@
+//1. Check if a Number is a Palindrome
+function isPalindrome(num) {
+  const originalNumber = num;
+  const digits = [];
+  while (num > 0) {
+    digits.unshift(num % 10);
+    num = Math.floor(num / 10);
+  }
+  let reveredNumber = 0;
+
+  for (let i = 0; i < digits.length; i++) {
+    reveredNumber += digits[i] * 10 ** i;
+  }
+
+  if (reveredNumber === originalNumber) {
+    console.log(`${originalNumber} is a Palindrom Number`);
+  } else {
+    console.log(`${originalNumber} is not a Palindrom Number`);
+  }
+}
+
+//2. Check if a Number is an Armstrong Number (also called a Narcissistic Number)
+function isArmStrong(num) {
+  const originalNumber = num;
+  const digits = [];
+  while (num > 0) {
+    digits.unshift(num % 10);
+    num = Math.floor(num / 10);
+  }
+  let sum = 0;
+
+  for (let i = 0; i < digits.length; i++) {
+    let n = digits[i];
+    sum += n ** digits.length;
+  }
+
+  if (sum === originalNumber) {
+    console.log(`${originalNumber} is an Armstrong Number`);
+  } else {
+    console.log(`${originalNumber} is not an Armstrong Number`);
+  }
+}
+
+/*********Homework Questions********/
+
+//1. Find the Sum of Digits
+function sumOfDigits(num) {
+  let sum = 0;
+  while (num > 0) {
+    let rem = num % 10;
+    sum += rem;
+    num = Math.floor(num / 10);
+  }
+
+  console.log(`Sum of Digits = ${sum}`);
+}
+
+//2. Find the Average of Digits
+function averageOfDigits(num) {
+  let sum = 0;
+  let numArr = [];
+
+  while (num > 0) {
+    let rem = num % 10;
+    numArr.push(rem);
+    sum += rem;
+    num = Math.floor(num / 10);
+  }
+
+  const average = sum / numArr.length;
+
+  console.log(`Average of Digits = ${average.toFixed(2)}`);
+}
+
+//3. Find the Largest and Smallest Digit in a Number
+function largestSmallestDigits(num) {
+  let smallest = 9;
+  let largest = 0;
+
+  while (num > 0) {
+    let rem = num % 10;
+    if (rem < smallest) smallest = rem;
+    if (rem > largest) largest = rem;
+    num = Math.floor(num / 10);
+  }
+
+  console.log(`Smallest digit = ${smallest} and Largest Digits = ${largest}`);
+}
+
+//4. Check if a Number is a Strong Number
+function checkStrongNum(n) {
+  const num = n;
+  let sum = 0;
+  while (n > 0) {
+    let rem = n % 10;
+    let fact = 1;
+    for (let i = 1; i <= rem; i++) {
+      fact = fact * i;
+    }
+    sum += fact;
+    n = Math.floor(n / 10);
+  }
+
+  if (sum === num) {
+    console.log(`${num} is a Strong number`);
+  } else {
+    console.log(`${num} is not a Strong number`);
+  }
+}
+
+//5. Check if a Number is an Automorphic Number
+function checkAutomorphicNum(n) {
+  const copy = n;
+  const sqr = n * n;
+  let count = 0;
+  while (n > 0) {
+    count++;
+    n = Math.floor(n / 10);
+  }
+
+  if (sqr % Math.pow(10, count) === copy) {
+    console.log(`${copy} is an Automorphic number`);
+  } else {
+    console.log(`${copy} is not an Automorphic number`);
+  }
+}
+
+//6. Find the Frequency of Each Digit
+function digitFrequency(num) {
+  const freq = {};
+
+  while (num > 0) {
+    let digit = num % 10;
+
+    if (freq[digit]) {
+      freq[digit]++;
+    } else {
+      freq[digit] = 1;
+    }
+
+    num = Math.floor(num / 10);
+  }
+
+  const keys = Object.keys(freq);
+
+  for (let i = 0; i < keys.length; i++) {
+    let key = keys[i];
+    console.log(`${key} → ${freq[key]}`);
+  }
+}
+
+//7. Check if a Number is a Harshad Number
+function isHarshadNumber(num) {
+  let n = num;
+  let digitSum = 0;
+
+  while (n > 0) {
+    let rem = n % 10;
+    digitSum += rem;
+    n = Math.floor(n / 10);
+  }
+
+  if (num % digitSum === 0) console.log("Harshad Number");
+  else console.log("Not a Harshad Number");
+}
+
+//Functions Call
+isPalindrome(121);
+isArmStrong(153);
+
+sumOfDigits(987);
+averageOfDigits(4567);
+largestSmallestDigits(9483);
+checkStrongNum(145);
+checkAutomorphicNum(25);
+digitFrequency(112233);
+isHarshadNumber(18);
